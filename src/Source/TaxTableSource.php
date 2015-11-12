4fae9366625133d4c1ac88dc71b948d16427f941
Ben Thomson <ben@abweb.com.au>
2015-11-12 10:45:05 +0800

    Adding attributes for source interface

--- src/Source/TaxTableSource.php
+++ src/Source/TaxTableSource.php
@@ -3,6 +3,21 @@ namespace ABWeb\IncomeTax\Source;
 
 interface TaxTableSource
 {
-    public function coefficients($scale = null);
-    public function leapYearPayments($scale = null);
+    public function coefficients(
+        $amountBeforeTax = null,
+        $scale = 2,
+        $claimedHelp = false,
+        $claimedSfss = false,
+        $seniorOffset = false,
+        $seniorOffsetType = null,
+        $medicareAdjustment = null,
+        $variation = null
+    );
+
+    public function determineScale(
+        $tfnProvided = true,
+        $taxFreeThreshold = true,
+        $foreignResident = false,
+        $medicareLevyExcemption = 'none'
+    );
 }
