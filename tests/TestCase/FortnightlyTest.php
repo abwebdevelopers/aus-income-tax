856e9e63cbb6bb8967c3609edb65750fe093d308
Ben Thomson <ben@abweb.com.au>
2015-11-13 17:43:03 +0800

    Adding additional source tests and fortnightly tests

--- tests/TestCase/FortnightlyTest.php
+++ tests/TestCase/FortnightlyTest.php
@@ -59,6 +59,102 @@ class FortnightlyTest extends \PHPUnit_Framework_TestCase
             6222 => 2238,
             6920 => 2580,
             6922 => 2582
+        ],
+        '2' => [ // Scale 2 - With tax-free threshold
+            88 => 0,
+            90 => 0,
+            232 => 0,
+            234 => 0,
+            498 => 0,
+            500 => 0,
+            708 => 0,
+            710 => 0,
+            720 => 2,
+            722 => 2,
+            788 => 16,
+            790 => 16,
+            984 => 72,
+            986 => 72,
+            1318 => 142,
+            1320 => 142,
+            1420 => 164,
+            1422 => 164,
+            1650 => 244,
+            1652 => 244,
+            1862 => 318,
+            1864 => 318,
+            2374 => 496,
+            2376 => 496,
+            2562 => 560,
+            2564 => 562,
+            3074 => 738,
+            3076 => 738,
+            3688 => 976,
+            3690 => 978,
+            4238 => 1192,
+            4240 => 1192,
+            4980 => 1480,
+            4982 => 1482,
+            5304 => 1606,
+            5306 => 1608,
+            5472 => 1672,
+            5474 => 1674,
+            5796 => 1798,
+            5798 => 1800,
+            5826 => 1810,
+            5828 => 1812,
+            6220 => 1964,
+            6222 => 1964,
+            6920 => 2238,
+            6922 => 2238
+        ],
+        '3' => [ // Scale 3 - Foreign resident
+            88 => 28,
+            90 => 30,
+            232 => 76,
+            234 => 76,
+            498 => 162,
+            500 => 162,
+            708 => 230,
+            710 => 230,
+            720 => 234,
+            722 => 234,
+            788 => 256,
+            790 => 256,
+            984 => 320,
+            986 => 320,
+            1318 => 428,
+            1320 => 428,
+            1420 => 462,
+            1422 => 462,
+            1650 => 536,
+            1652 => 536,
+            1862 => 606,
+            1864 => 606,
+            2374 => 772,
+            2376 => 772,
+            2562 => 832,
+            2564 => 834,
+            3074 => 1000,
+            3076 => 1000,
+            3688 => 1226,
+            3690 => 1228,
+            4238 => 1430,
+            4240 => 1432,
+            4980 => 1704,
+            4982 => 1706,
+            5304 => 1824,
+            5306 => 1826,
+            5472 => 1886,
+            5474 => 1888,
+            5796 => 2006,
+            5798 => 2008,
+            5826 => 2018,
+            5828 => 2018,
+            6220 => 2164,
+            6222 => 2164,
+            6920 => 2422,
+            6922 => 2424
         ]
     ];
 
@@ -74,6 +170,22 @@ class FortnightlyTest extends \PHPUnit_Framework_TestCase
         foreach ($this->data[1] as $earnings => $expectedTax) {
             $tax = $this->IncomeTax->calculateTax($earnings, 'fortnightly', '2015-06-02', 'standard', 1);
             $this->assertEquals($expectedTax, $tax, 'Scale 1 - Fortnightly Earnings: ' . $earnings);
+        }
+    }
+
+    public function testScaleTwo()
+    {
+        foreach ($this->data[2] as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'fortnightly', '2015-06-02', 'standard', 2);
+            $this->assertEquals($expectedTax, $tax, 'Scale 2 - Fortnightly Earnings: ' . $earnings);
+        }
+    }
+
+    public function testScaleThree()
+    {
+        foreach ($this->data[3] as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'fortnightly', '2015-06-02', 'standard', 3);
+            $this->assertEquals($expectedTax, $tax, 'Scale 3 - Fortnightly Earnings: ' . $earnings);
         }
     }
 }
--- tests/TestCase/Source/ATOExcelSourceTest.php
+++ tests/TestCase/Source/ATOExcelSourceTest.php
@@ -18,6 +18,61 @@ class ATOExcelSourceTest extends \PHPUnit_Framework_TestCase
         ]);
     }
 
+    /**
+    * @expectedException \ABWeb\IncomeTax\Exception\SourceException
+    * @expectedExceptionCode 31250
+    **/
+    public function testMissingSource()
+    {
+        $this->ATOExcelSource->loadStandardFile(dirname(dirname(__DIR__)) . DS . 'ext' . DS . 'MISSING_FILE.xlsx');
+    }
+
+    /**
+    * @expectedException \ABWeb\IncomeTax\Exception\SourceException
+    * @expectedExceptionCode 31251
+    **/
+    public function testInvalidSource()
+    {
+        $this->ATOExcelSource->loadStandardFile(dirname(dirname(__DIR__)) . DS . 'ext' . DS . 'NAT_INVALID.txt');
+    }
+
+    /**
+    * @expectedException \ABWeb\IncomeTax\Exception\SourceException
+    * @expectedExceptionCode 31253
+    **/
+    public function testInvalidSourceRows()
+    {
+        $this->ATOExcelSource->loadStandardFile(dirname(dirname(__DIR__)) . DS . 'ext' . DS . 'NAT_INVALID.xlsx');
+    }
+
+    public function testCoefficientsArrayParameter()
+    {
+        $coefficients = $this->ATOExcelSource->coefficients([
+            'amountBeforeTax' => 1000,
+            'type' => 'standard',
+            'scale' => 1
+        ]);
+
+        $this->assertEquals([
+            'percentage' => 0.3450,
+            'subtraction' => 41.1734
+        ], $coefficients);
+    }
+
+    public function testCoefficientsInvalidTypeAndScale()
+    {
+        $this->assertFalse($this->ATOExcelSource->coefficients([
+            'amountBeforeTax' => 1000,
+            'type' => 'not-exist',
+            'scale' => 1
+        ]));
+        $this->assertFalse($this->ATOExcelSource->coefficients([
+            'amountBeforeTax' => 1000,
+            'type' => 'standard',
+            'scale' => 9
+        ]));
+    }
+
     public function testNoTFN()
     {
         $scale = $this->ATOExcelSource->determineScale([
@@ -55,5 +110,138 @@ class ATOExcelSourceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('3', $scale['scale']);
     }
 
+    public function testNoTaxFreeThreshold()
+    {
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'taxFreeThreshold' => false
+        ]);
 
+        $this->assertEquals('standard', $scale['type']);
+        $this->assertEquals('1', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'foreignResident' => true,
+            'tfnProvided' => true,
+            'taxFreeThreshold' => false
+        ]);
+
+        $this->assertEquals('standard', $scale['type']);
+        $this->assertEquals('3', $scale['scale']);
+    }
+
+    public function testDebts()
+    {
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'helpDebt' => true
+        ]);
+
+        $this->assertEquals('help', $scale['type']);
+        $this->assertEquals('2', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'sfssDebt' => true
+        ]);
+
+        $this->assertEquals('sfss', $scale['type']);
+        $this->assertEquals('2', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'sfssDebt' => true
+        ]);
+
+        $this->assertEquals('combo', $scale['type']);
+        $this->assertEquals('2', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'foreignResident' => true,
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'sfssDebt' => true
+        ]);
+
+        $this->assertEquals('combo', $scale['type']);
+        $this->assertEquals('3', $scale['scale']);
+
+        // Seniors with HELP and SFSS debt still get charged seniors tax rate
+        $scale = $this->ATOExcelSource->determineScale([
+            'foreignResident' => true,
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'sfssDebt' => true,
+            'seniorsOffset' => 'single'
+        ]);
+
+        $this->assertEquals('seniors', $scale['type']);
+        $this->assertEquals('single', $scale['scale']);
+    }
+
+    public function testSeniors()
+    {
+        $scale = $this->ATOExcelSource->determineScale([
+            'seniorsOffset' => 'single'
+        ]);
+
+        $this->assertEquals('seniors', $scale['type']);
+        $this->assertEquals('single', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'seniorsOffset' => 'illness-separated'
+        ]);
+
+        $this->assertEquals('seniors', $scale['type']);
+        $this->assertEquals('illness-separated', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'seniorsOffset' => 'couple'
+        ]);
+
+        $this->assertEquals('seniors', $scale['type']);
+        $this->assertEquals('member of a couple', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => false,
+            'seniorsOffset' => 'illness-separated'
+        ]);
+
+        $this->assertEquals('standard', $scale['type']);
+        $this->assertEquals('4 resident', $scale['scale']);
+    }
+
+    public function testMedicareLevy()
+    {
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'medicareLevyExemption' => 'full'
+        ]);
+
+        $this->assertEquals('help', $scale['type']);
+        $this->assertEquals('5', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'sfssDebt' => true,
+            'medicareLevyExemption' => 'half'
+        ]);
+
+        $this->assertEquals('combo', $scale['type']);
+        $this->assertEquals('6', $scale['scale']);
+
+        $scale = $this->ATOExcelSource->determineScale([
+            'foreignResident' => true,
+            'tfnProvided' => true,
+            'helpDebt' => true,
+            'sfssDebt' => true,
+            'medicareLevyExemption' => 'half'
+        ]);
+
+        $this->assertEquals('combo', $scale['type']);
+        $this->assertEquals('6', $scale['scale']);
+    }
 }
