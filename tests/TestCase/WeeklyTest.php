613839a47e3350f46c762fe905619ec8a41c6ca2
Ben Thomson <ben@abweb.com.au>
2019-03-11 16:11:03 +0800

    Update weekly test cases

--- tests/TestCase/WeeklyTest.php
+++ tests/TestCase/WeeklyTest.php
@@ -1,68 +1,104 @@
 <?php
-namespace ABWeb\IncomeTax\Tests\TestCase;
+namespace ABWebDevelopers\AusIncomeTax\Tests\TestCase;
 
-use ABWeb\IncomeTax\IncomeTax;
-use ABWeb\IncomeTax\Source\ATOExcelSource;
+use PHPUnit\Framework\TestCase;
+use ABWebDevelopers\AusIncomeTax\IncomeTax;
+use ABWebDevelopers\AusIncomeTax\Source\ATOExcelSource;
 
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
-
-class WeeklyTest extends \PHPUnit_Framework_TestCase
+class WeeklyTest extends TestCase
 {
-    // Values taken from https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Schedule-1-Calculating-amounts-to-be-withheld-2015-16.pdf
-    protected $data = [
-        '1' => [ // Scale 1 - No tax-free threshold
-            44 => 8,
-            45 => 9,
-            116 => 25,
-            117 => 25,
-            249 => 56,
+    public function setUp(): void
+    {
+        if (!defined('DS')) {
+            define('DS', $_ENV['DIRECTORY_SEPARATOR'] ?? '/');
+        }
+
+        $taxTableDir = implode(DS, [
+            dirname(dirname(__DIR__)),
+            'resources',
+            'tax-tables',
+            '2018-19',
+        ]) . DS;
+
+        $this->IncomeTax = new IncomeTax(new ATOExcelSource([
+            'standardFile' => $taxTableDir . 'NAT_1004_2018.xlsx',
+        ]));
+    }
+
+    /**
+     * Test Scale 1 withheld amounts - Where Tax Free Threshold is not claimed
+     *
+     * Values taken from ATO's "Statement of formulas for calculating amounts to be withheld 2018-19" PDF
+     * https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Calculating-amounts-to-be-withheld-2018-19.pdf
+     */
+    public function testScaleOne()
+    {
+        $data = [
+            71 => 13,
+            72 => 14,
+            116 => 24,
+            117 => 24,
+            249 => 55,
             250 => 56,
             354 => 80,
-            355 => 81,
-            360 => 82,
+            355 => 80,
+            360 => 81,
             361 => 82,
-            394 => 94,
-            395 => 94,
-            492 => 128,
-            493 => 128,
-            659 => 186,
-            660 => 186,
-            710 => 204,
-            711 => 204,
-            825 => 244,
-            826 => 244,
+            421 => 102,
+            422 => 103,
+            527 => 139,
+            528 => 140,
+            710 => 203,
+            711 => 203,
+            712 => 204,
+            713 => 204,
+            890 => 266,
+            891 => 266,
             931 => 280,
-            932 => 281,
-            1187 => 369,
-            1188 => 369,
-            1281 => 405,
-            1282 => 406,
-            1537 => 505,
-            1538 => 506,
-            1844 => 625,
-            1845 => 625,
-            2119 => 732,
-            2120 => 733,
-            2490 => 877,
-            2491 => 877,
-            2652 => 940,
-            2653 => 940,
-            2736 => 973,
-            2737 => 973,
-            2898 => 1036,
-            2899 => 1036,
-            2913 => 1042,
-            2914 => 1042,
-            3110 => 1119,
-            3111 => 1119,
-            3460 => 1290,
-            3461 => 1291
-        ],
-        '2' => [ // Scale 2 - With tax-free threshold
-            44 => 0,
-            45 => 0,
+            932 => 280,
+            1281 => 401,
+            1282 => 401,
+            1379 => 434,
+            1380 => 435,
+            1729 => 571,
+            1730 => 571,
+            1844 => 616,
+            1845 => 616,
+            2119 => 723,
+            2120 => 723,
+            2490 => 868,
+            2491 => 868,
+            2652 => 931,
+            2653 => 931,
+            2736 => 964,
+            2737 => 964,
+            2898 => 1027,
+            2899 => 1027,
+            2913 => 1033,
+            2914 => 1033,
+            3110 => 1109,
+            3111 => 1110,
+            3460 => 1274,
+            3461 => 1274
+        ];
+
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', 1);
+            $this->assertEquals($expectedTax, $tax, 'Scale 1 - Weekly Earnings: ' . $earnings);
+        }
+    }
+
+    /**
+     * Test Scale 2 withheld amounts - Where Tax Free Threshold is not claimed
+     *
+     * Values taken from ATO's "Statement of formulas for calculating amounts to be withheld 2018-19" PDF
+     * https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Calculating-amounts-to-be-withheld-2018-19.pdf
+     */
+    public function testScaleTwo()
+    {
+        $data = [
+            71 => 0,
+            72 => 0,
             116 => 0,
             117 => 0,
             249 => 0,
@@ -71,46 +107,61 @@ class WeeklyTest extends \PHPUnit_Framework_TestCase
             355 => 0,
             360 => 1,
             361 => 1,
-            394 => 8,
-            395 => 8,
-            492 => 36,
-            493 => 36,
-            659 => 71,
-            660 => 71,
+            421 => 13,
+            422 => 13,
+            527 => 43,
+            528 => 44,
             710 => 82,
             711 => 82,
-            825 => 122,
-            826 => 122,
+            712 => 82,
+            713 => 83,
+            890 => 144,
+            891 => 145,
             931 => 159,
             932 => 159,
-            1187 => 248,
-            1188 => 248,
             1281 => 280,
             1282 => 281,
-            1537 => 369,
-            1538 => 369,
-            1844 => 488,
-            1845 => 489,
-            2119 => 596,
-            2120 => 596,
-            2490 => 740,
-            2491 => 741,
-            2652 => 803,
-            2653 => 804,
-            2736 => 836,
-            2737 => 837,
-            2898 => 899,
-            2899 => 900,
-            2913 => 905,
-            2914 => 906,
-            3110 => 982,
-            3111 => 982,
-            3460 => 1119,
-            3461 => 1119
-        ],
-        '3' => [ // Scale 3 - Foreign residents
-            44 => 14,
-            45 => 15,
+            1379 => 314,
+            1380 => 314,
+            1729 => 435,
+            1730 => 435,
+            1844 => 480,
+            1845 => 480,
+            2119 => 587,
+            2120 => 587,
+            2490 => 732,
+            2491 => 732,
+            2652 => 795,
+            2653 => 795,
+            2736 => 828,
+            2737 => 828,
+            2898 => 891,
+            2899 => 891,
+            2913 => 897,
+            2914 => 897,
+            3110 => 973,
+            3111 => 974,
+            3460 => 1110,
+            3461 => 1110
+        ];
+
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', 2);
+            $this->assertEquals($expectedTax, $tax, 'Scale 2 - Weekly Earnings: ' . $earnings);
+        }
+    }
+
+    /**
+     * Test Scale 3 withheld amounts - Foreign resident
+     *
+     * Values taken from ATO's "Statement of formulas for calculating amounts to be withheld 2018-19" PDF
+     * https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Calculating-amounts-to-be-withheld-2018-19.pdf
+     */
+    public function testScaleThree()
+    {
+        $data = [
+            71 => 23,
+            72 => 23,
             116 => 38,
             117 => 38,
             249 => 81,
@@ -119,201 +170,200 @@ class WeeklyTest extends \PHPUnit_Framework_TestCase
             355 => 115,
             360 => 117,
             361 => 117,
-            394 => 128,
-            395 => 128,
-            492 => 160,
-            493 => 160,
-            659 => 214,
-            660 => 214,
+            421 => 137,
+            422 => 137,
+            527 => 171,
+            528 => 172,
             710 => 231,
             711 => 231,
-            825 => 268,
-            826 => 268,
+            712 => 231,
+            713 => 232,
+            890 => 289,
+            891 => 290,
             931 => 303,
             932 => 303,
-            1187 => 386,
-            1188 => 386,
             1281 => 416,
             1282 => 417,
-            1537 => 500,
-            1538 => 500,
-            1844 => 613,
-            1845 => 614,
-            2119 => 715,
-            2120 => 716,
-            2490 => 852,
-            2491 => 853,
-            2652 => 912,
-            2653 => 913,
-            2736 => 943,
-            2737 => 944,
-            2898 => 1003,
-            2899 => 1004,
-            2913 => 1009,
-            2914 => 1009,
-            3110 => 1082,
-            3111 => 1082,
-            3460 => 1211,
-            3461 => 1212
-        ],
-        '4' => [ // Scale 4 - No TFN provided
-            44 => 21,
-            249 => 122,
-            492 => 241,
-            825 => 404,
-            1538 => 753,
-            2652 => 1299,
-            3460 => 1695
-        ],
-        '5' => [ // Scale 5 - Full Medicare Levy
-            44 => 0,
-            45 => 0,
-            116 => 0,
-            117 => 0,
-            249 => 0,
-            250 => 0,
-            354 => 0,
-            355 => 0,
-            360 => 1,
-            361 => 1,
-            394 => 8,
-            395 => 8,
-            492 => 26,
-            493 => 26,
-            659 => 58,
-            660 => 58,
-            710 => 68,
-            711 => 68,
-            825 => 105,
-            826 => 106,
-            931 => 140,
-            932 => 140,
-            1187 => 224,
-            1188 => 224,
-            1281 => 255,
-            1282 => 255,
-            1537 => 338,
-            1538 => 338,
-            1844 => 451,
-            1845 => 452,
-            2119 => 553,
-            2120 => 554,
-            2490 => 690,
-            2491 => 691,
-            2652 => 750,
-            2653 => 751,
-            2736 => 781,
-            2737 => 782,
-            2898 => 841,
-            2899 => 842,
-            2913 => 847,
-            2914 => 847,
-            3110 => 920,
-            3111 => 920,
-            3460 => 1049,
-            3461 => 1050
-        ],
-        '6' => [ // Scale 6 - Full Medicare Levy
-            44 => 0,
-            45 => 0,
-            116 => 0,
-            117 => 0,
-            249 => 0,
-            250 => 0,
-            354 => 0,
-            355 => 0,
-            360 => 1,
-            361 => 1,
-            394 => 8,
-            395 => 8,
-            492 => 26,
-            493 => 26,
-            659 => 58,
-            660 => 58,
-            710 => 70,
-            711 => 70,
-            825 => 113,
-            826 => 114,
-            931 => 149,
-            932 => 150,
-            1187 => 236,
-            1188 => 236,
-            1281 => 267,
-            1282 => 268,
-            1537 => 353,
-            1538 => 354,
-            1844 => 470,
-            1845 => 470,
-            2119 => 574,
-            2120 => 575,
-            2490 => 715,
-            2491 => 716,
-            2652 => 777,
-            2653 => 777,
-            2736 => 809,
-            2737 => 809,
-            2898 => 870,
-            2899 => 871,
-            2913 => 876,
-            2914 => 876,
-            3110 => 951,
-            3111 => 951,
-            3460 => 1084,
-            3461 => 1084
-        ]
-    ];
+            1379 => 448,
+            1380 => 448,
+            1729 => 562,
+            1730 => 563,
+            1844 => 605,
+            1845 => 605,
+            2119 => 707,
+            2120 => 707,
+            2490 => 844,
+            2491 => 844,
+            2652 => 904,
+            2653 => 904,
+            2736 => 935,
+            2737 => 935,
+            2898 => 995,
+            2899 => 995,
+            2913 => 1000,
+            2914 => 1001,
+            3110 => 1073,
+            3111 => 1074,
+            3460 => 1203,
+            3461 => 1203
+        ];
 
-    public function setUp()
-    {
-        $this->IncomeTax = new IncomeTax(new ATOExcelSource([
-            'standardFile' => dirname(dirname(__DIR__)) . DS . 'ext' . DS . '2015' . DS . 'NAT_1004_0.xlsx',
-        ]));
-    }
-
-    public function testScaleOne()
-    {
-        foreach ($this->data[1] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', 1);
-            $this->assertEquals($expectedTax, $tax, 'Scale 1 - Weekly Earnings: ' . $earnings);
-        }
-    }
-
-    public function testScaleTwo()
-    {
-        foreach ($this->data[2] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', 2);
-            $this->assertEquals($expectedTax, $tax, 'Scale 2 - Weekly Earnings: ' . $earnings);
-        }
-    }
-
-    public function testScaleThree()
-    {
-        foreach ($this->data[3] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', 3);
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', 3);
             $this->assertEquals($expectedTax, $tax, 'Scale 3 - Weekly Earnings: ' . $earnings);
         }
     }
 
+    /**
+     * Test Scale 4 withheld amounts - Where the TFN is not provided
+     *
+     * As the taxation rate is flat, we are only testing a subset of income amounts
+     */
     public function testScaleFour()
     {
-        foreach ($this->data[4] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', '4 resident');
+        $data = [
+            71 => 33,
+            249 => 117,
+            360 => 169,
+            527 => 247,
+            712 => 334,
+            931 => 437,
+            1379 => 648,
+            1844 => 866,
+            2490 => 1170,
+            2736 => 1285,
+            2913 => 1369,
+            3460 => 1626,
+        ];
+
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', '4 resident');
             $this->assertEquals($expectedTax, $tax, 'Scale 4 - Weekly Earnings: ' . $earnings);
         }
     }
 
+    /**
+     * Test Scale 5 withheld amounts - Full Medicare exemption
+     *
+     * Values taken from ATO's "Statement of formulas for calculating amounts to be withheld 2018-19" PDF
+     * https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Calculating-amounts-to-be-withheld-2018-19.pdf
+     */
     public function testScaleFive()
     {
-        foreach ($this->data[5] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', 5);
+        $data = [
+            71 => 0,
+            72 => 0,
+            116 => 0,
+            117 => 0,
+            249 => 0,
+            250 => 0,
+            354 => 0,
+            355 => 0,
+            360 => 1,
+            361 => 1,
+            421 => 13,
+            422 => 13,
+            527 => 33,
+            528 => 33,
+            710 => 68,
+            711 => 68,
+            712 => 68,
+            713 => 69,
+            890 => 127,
+            891 => 127,
+            931 => 140,
+            932 => 140,
+            1281 => 255,
+            1282 => 255,
+            1379 => 287,
+            1380 => 287,
+            1729 => 400,
+            1730 => 401,
+            1844 => 443,
+            1845 => 443,
+            2119 => 545,
+            2120 => 545,
+            2490 => 682,
+            2491 => 682,
+            2652 => 742,
+            2653 => 742,
+            2736 => 773,
+            2737 => 773,
+            2898 => 833,
+            2899 => 833,
+            2913 => 838,
+            2914 => 839,
+            3110 => 911,
+            3111 => 912,
+            3460 => 1041,
+            3461 => 1041
+        ];
+
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', 5);
             $this->assertEquals($expectedTax, $tax, 'Scale 5 - Weekly Earnings: ' . $earnings);
         }
     }
 
+    /**
+     * Test Scale 6 withheld amounts - Half Medicare exemption
+     *
+     * Values taken from ATO's "Statement of formulas for calculating amounts to be withheld 2018-19" PDF
+     * https://www.ato.gov.au/uploadedFiles/Content/MEI/downloads/Calculating-amounts-to-be-withheld-2018-19.pdf
+     */
     public function testScaleSix()
     {
-        foreach ($this->data[6] as $earnings => $expectedTax) {
-            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2015-06-02', 'standard', 6);
+        $data = [
+            71 => 0,
+            72 => 0,
+            116 => 0,
+            117 => 0,
+            249 => 0,
+            250 => 0,
+            354 => 0,
+            355 => 0,
+            360 => 1,
+            361 => 1,
+            421 => 13,
+            422 => 13,
+            527 => 33,
+            528 => 33,
+            710 => 68,
+            711 => 68,
+            712 => 68,
+            713 => 69,
+            890 => 135,
+            891 => 136,
+            931 => 149,
+            932 => 150,
+            1281 => 267,
+            1282 => 268,
+            1379 => 300,
+            1380 => 301,
+            1729 => 418,
+            1730 => 418,
+            1844 => 461,
+            1845 => 462,
+            2119 => 566,
+            2120 => 566,
+            2490 => 707,
+            2491 => 707,
+            2652 => 768,
+            2653 => 769,
+            2736 => 800,
+            2737 => 801,
+            2898 => 862,
+            2899 => 862,
+            2913 => 867,
+            2914 => 868,
+            3110 => 942,
+            3111 => 943,
+            3460 => 1075,
+            3461 => 1076
+        ];
+
+        foreach ($data as $earnings => $expectedTax) {
+            $tax = $this->IncomeTax->calculateTax($earnings, 'weekly', '2018-06-02', 'standard', 6);
             $this->assertEquals($expectedTax, $tax, 'Scale 6 - Weekly Earnings: ' . $earnings);
         }
     }
